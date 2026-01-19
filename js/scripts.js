3d7ffbbb42a3d951491cda3cb92acd3b03b38c49
Alexsandre Mira <130816145+jun1mira@users.noreply.github.com>
2026-01-19 18:06:09 -0300

    Refatora código: remove variáveis não utilizadas, corrige typos e melhora formatação

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 import { WEATHER_API_KEY } from "./config.js";
 
-const apiKey = WEATHER_API_KEY;
-const apiCountryURL = "https://flagsapi.com/";
-
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
-const wheatherIconnElement = document.querySelector("#weather-icon");
+const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
 const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
@@ -17,7 +14,7 @@ const windElement = document.querySelector("#wind span");
 const weatherContainer = document.querySelector("#weather-data");
 
 const getWeatherData = async (city) => {
-  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${WEATHER_API_KEY}&lang=pt_br`;
 
   const res = await fetch(apiWeatherURL);
   const data = await res.json();
@@ -36,7 +33,7 @@ const showWeatherData = async (city) => {
   tempElement.innerText = parseInt(data.main.temp);
   descElement.innerText = data.weather[0].description;
 
-  wheatherIconnElement.setAttribute(
+  weatherIconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
   );
